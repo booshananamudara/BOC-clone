f5ac56906168ed4097e3586fa1676bca15a5e1d6
Nipun2001M <108778265+Nipun2001M@users.noreply.github.com>
2022-07-18 11:13:11 +0530

    I created Banking Museum page
    I created banking museum page using HTML ,Css and JavaScript

---
+++ js/bankingmuseum.js
@@ -0,0 +1,29 @@
+
+
+function hide()
+{
+    
+    if (document.getElementById("nipun").style.display == "none") {
+        document.getElementById("nipun").style.display = "block";
+        document.getElementById("hh").textContent="+";
+
+        
+
+
+    } 
+    else {
+        document.getElementById("nipun").style.display = "none";
+        document.getElementById("hh").textContent="-";
+
+
+
+    };
+
+   
+
+
+
+
+
+
+}
